d1ec86f872ae22815d1283872ead08469d2b21b7
Laileanna Rachelle R. Del Rosario <lrdelrosario@udm.edu.ph>
2026-04-14 14:21:21 +0800

    index

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Lai's Portfolio</title>
+<link rel="icon" type="image/png" href="assets/images/logo.png">
 <link rel="stylesheet" href="assets/style.css">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
